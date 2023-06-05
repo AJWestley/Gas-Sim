23f7ab78142b4ec3325014d9ffe46322f361d3cf
AJ <alexanderjwestley@gmail.com>
2023-06-05 17:52:39 +0200

    MORE BUTTONS

--- button.py
+++ button.py
@@ -119,3 +119,47 @@ class TextInput:
         if len(self.value) <= 1: self.value = '0'
         else: 
             self.value = self.value[:len(self.value)-1]
+            
+class IntensityButton:
+    
+    def __init__(self, x, y, width, height, text, values, displays, border_thickness=0, font_size=24, fill_colour=(192, 244, 255),
+                 border_colour=(5, 5, 5), hover_colour=(200, 170, 170), text_mult=0.23):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.text = text
+        l = min(len(values), len(displays))
+        self.values = values[:l]
+        self.displays = displays[:l]
+        self.current_val = 0
+        self.border_thickness = border_thickness
+        self.font_size = font_size
+        self.fill = fill_colour
+        self.border = border_colour
+        self.hover = hover_colour
+        self.active_fill = fill_colour
+        self.tm = text_mult
+
+    def check_over(self, x, y):
+        if self.x <= x <= self.x + self.width and self.y <= y <= self.y + self.height:
+            self.active_fill = self.hover
+            return True
+        self.active_fill = self.fill
+        return False
+    
+    def increment(self):
+        self.current_val += 1
+        if self.current_val >= len(self.values): self.current_val = 0
+
+    def draw(self, pg, screen):
+        pg.draw.rect(screen, self.border, (self.x, self.y, self.width, self.height))
+        pg.draw.rect(screen, self.active_fill,
+                     (self.x + self.border_thickness, self.y + self.border_thickness,
+                      self.width - 2 * self.border_thickness, self.height - 2 * self.border_thickness))
+        font = pg.font.Font(None, self.font_size)
+        text = font.render(self.text, True, self.border)
+        screen.blit(text, (self.x - self.tm * self.width, self.y + 0.3 * self.height))
+        st = self.displays[self.current_val]
+        text = font.render(st, True, self.border)
+        screen.blit(text, (self.x + 0.2 * self.width, self.y + 0.18 * self.height))
--- gui.py
+++ gui.py
@@ -1,5 +1,5 @@
 import pygame as pg
-from button import Button, CheckBox, TextInput
+from button import Button, CheckBox, TextInput, IntensityButton
 from colours import BLACK, WHITE, RED, GREEN, BLUE
 import particle
 from collisions import SpatialHashMap
@@ -7,17 +7,18 @@ from collisions import SpatialHashMap
 # ---------- Initialisation ---------- #
 WIDTH, HEIGHT = 800, 600
 
-box = [250, 750, 50, 550]
-particle_radius = 3
-particle_list = []
-particle_map = SpatialHashMap(box, int(particle_radius * 1.5))
-
 pause_button = Button(50, 50, 120, 40, "Pause/Play", 3, 24, text_mult=0.15, border_colour=WHITE, fill_colour=(30, 30, 40), hover_colour=(60, 60, 80))
 clear_button = Button(50, 120, 120, 40, "Clear", 3, 24, text_mult=0.3, border_colour=WHITE, fill_colour=(30, 30, 40), hover_colour=(60, 60, 80))
-particle_num_input = TextInput(85, 300, 80, 35, "Number of particles", 3, 24, border_colour=WHITE, fill_colour=(30, 30, 40), hover_colour=(60, 60, 80))
-ideal_check = CheckBox(140, 350, 25, "Ideal", 3, 24, text_mult=1.8, border_colour=WHITE, fill_colour=(30, 30, 40), hover_colour=(60, 60, 80))
-highlight_button = CheckBox(140, 400, 25, "Highlight", 3, 24, text_mult=3, border_colour=WHITE, fill_colour=(30, 30, 40), hover_colour=(60, 60, 80))
+particle_num_input = TextInput(85, 250, 80, 35, "Number of particles:", 3, 24, border_colour=WHITE, fill_colour=(30, 30, 40), hover_colour=(60, 60, 80))
+ideal_check = CheckBox(140, 300, 25, "Ideal:", 3, 24, text_mult=2, border_colour=WHITE, fill_colour=(30, 30, 40), hover_colour=(60, 60, 80))
+highlight_button = CheckBox(140, 350, 25, "Highlight:", 3, 24, text_mult=3.3, border_colour=WHITE, fill_colour=(30, 30, 40), hover_colour=(60, 60, 80))
+particle_radius_button = IntensityButton(100, 400, 65, 25, "Size:", [4, 8], ["small", "large"], 3, 24, text_mult=0.8, border_colour=WHITE, fill_colour=(30, 30, 40), hover_colour=(60, 60, 80))
+particle_speed_button = IntensityButton(100, 450, 65, 25, "Temp:", [10, 30], ["cold", "warm"], 3, 24, text_mult=0.8, border_colour=WHITE, fill_colour=(30, 30, 40), hover_colour=(60, 60, 80))
 create_button = Button(50, 500, 120, 40, "Create", 3, 24, text_mult=0.25, border_colour=WHITE, fill_colour=(30, 30, 40), hover_colour=(60, 60, 80))
+
+box = [250, 750, 50, 550]
+particle_list = []
+particle_map = SpatialHashMap(box, 9)
 
 pg.init()
 pg.display.set_caption('Gas Simulation')
@@ -50,15 +51,21 @@ while running:
             highlight_button.checked = not highlight_button.checked
         elif particle_num_input.check_over(m_x, m_y) and event.type == pg.MOUSEBUTTONDOWN:
             particle_num_input.active = True
+        elif particle_radius_button.check_over(m_x, m_y) and event.type == pg.MOUSEBUTTONDOWN:
+            particle_radius_button.increment()
+        elif particle_speed_button.check_over(m_x, m_y) and event.type == pg.MOUSEBUTTONDOWN:
+            particle_speed_button.increment()
         elif clear_button.check_over(m_x, m_y) and event.type == pg.MOUSEBUTTONDOWN:
             particle_list.clear()
             for bucket in particle_map.map:
                 bucket.clear()
         elif create_button.check_over(m_x, m_y) and event.type == pg.MOUSEBUTTONDOWN and len(particle_list) == 0:
             particle_count = int(particle_num_input.value)
-            ave_vel = 30
+            particle_radius = particle_radius_button.values[particle_radius_button.current_val]
+            ave_vel = particle_speed_button.values[particle_speed_button.current_val]
             hl = RED if highlight_button.checked else BLUE
-            particle_list = particle.generate_gas(particle_count, [280, 720, 80, 520], ave_vel, ideal_check.checked, BLUE, hl, radius=particle_radius)
+            particle_list = particle.generate_gas(particle_count, [300, 700, 100, 500], ave_vel, ideal_check.checked, BLUE, hl, radius=particle_radius)
+            particle_map = SpatialHashMap(box, int(particle_radius + 1))
             for p in particle_list:
                 particle_map.insert((p.x, p.y), p)
         elif event.type == pg.MOUSEBUTTONDOWN:
@@ -87,6 +94,8 @@ while running:
     ideal_check.draw(pg, screen)
     highlight_button.draw(pg, screen)
     particle_num_input.draw(pg, screen)
+    particle_radius_button.draw(pg, screen)
+    particle_speed_button.draw(pg, screen)
 
     pg.display.flip()
     # -------------------------------- #
